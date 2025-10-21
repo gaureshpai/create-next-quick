ab236af8541eb4806324420d4fd0238d80fd4209
gaureshpai <paigauresh@gmail.com>
2025-10-22 00:11:15 +0530

    fix: min req for node version increase from 18 to 20

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import chalk from "chalk";
 import { run, deleteFolder, createFolder, deleteFile, fileExists, writeFile } from './lib/utils.js';
 import { createPages, createLayout } from './lib/templates.js';
 
-const MIN_NODE_VERSION = 18;
+const MIN_NODE_VERSION = 20;
 const currentNodeVersion = process.versions.node;
 
 if (parseInt(currentNodeVersion.split('.')[0]) <= MIN_NODE_VERSION) {
